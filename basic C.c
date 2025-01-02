7287280a6b01811fcd02b247b4177359d6ddffc6
barun-lamsal32 <barunlamsal516@gmail.com>
2025-01-02 22:45:50 +0545

    Add files via upload

---
+++ basic C.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main () {
+    int a,b ;
+printf("Enter two integer ");
+scanf("%d %d",&a,&b);
+a+=b;
+printf("After adding = %d",a);
+getch ();
+    return 0;
+}
