1579689eba415e759462cbd55b49400e1489f91b
barun-lamsal32 <barunlamsal516@gmail.com>
2025-01-07 22:26:31 +0545

    Add files via upload

--- IO.c
+++ IO.c
@@ -1,42 +1,39 @@
 #include<stdio.h>
-#include<conio.h>
 
-int main () {
-    float x, y, result;
+int main() {
+
+    int a, b;
     char ch;
 
+    // Input two numbers
     printf("Enter two numbers: ");
-    scanf("%f %f", &x, &y);
+    scanf("%d%d", &a, &b);
 
-    getchar();
+    // Input operator
+    printf("Enter operator: ");
+    scanf(" %c", &ch);  // Space before %c to consume any leftover newline character
 
-    printf("Enter a mathematical operator (+, -, *, /): ");
-    scanf("%c", &ch);
 
-    if (ch == '*') {
-        result = x * y;
-        printf("\nResult is %.2f", result);
-    }
-    else if (ch == '+') {
-        result = x + y;
-        printf("\nResult is %.2f", result);
-    }
-    else if (ch == '-') {
-        result = x - y;
-        printf("\nResult is %.2f", result);
-    }
-    else if (ch == '/') {
-        if (y != 0) {
-            result = x / y;
-            printf("\nResult is %.2f", result);
-        } else {
-            printf("\nError! Division by zero.");
-        }
-    }
-    else {
-        printf("Invalid operator\n");
+    switch (ch) {
+        case '+':
+            printf("%d\n", a + b);
+            break;
+        case '-':
+            printf("%d\n", a - b);
+            break;
+        case '*':
+            printf("%d\n", a * b);
+            break;
+        case '/':
+            if (b != 0) {
+                printf("%d\n", a / b);
+            } else {
+                printf("Error: Division by zero\n");
+            }
+            break;
+        default:
+            printf("Invalid input\n");
     }
 
-    getch();
     return 0;
 }
