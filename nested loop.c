5a8ee5e39d90595bb5d7da453412b076db7a3ed0
barun-lamsal32 <barunlamsal516@gmail.com>
2025-01-12 18:50:37 +0545

    Add files via upload

---
+++ nested loop.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    int rows,a =0,b =1,c;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    for (int i = 1; i <= rows; i++) {
+
+        for (int j = 1; j <= i; j++) {
+
+            printf("%d ", a);
+
+
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        printf("\n");
+    }
+    return 0;
+}
