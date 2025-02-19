8354fa219c8de0680a0bac18d52cce9262502c12
barun-lamsal32 <barunlamsal516@gmail.com>
2025-02-19 13:25:49 +0545

    Create area of paralleogram.c

---
+++ area of paralleogram.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+	int b,h,a;
+	printf("Enter the base and height of paralleogram");
+	scanf("%d %d ",&b,&h);
+	a=(b*h);
+	printf("the area of paralleogram is %d ",a);
+	
+}
