264133f932797ecd2a09548b01dc73962a9ea2f9
barun-lamsal32 <barunlamsal516@gmail.com>
2025-02-19 23:05:02 +0545

    Create to read and print r cross c matrix.cpp

---
+++ to read and print r cross c matrix.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+main()
+{
+	int a[3][3],r,c,i,j;
+	printf("Enter the no of the rows and columns");
+	scanf("%d %d",&r,&c);
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			scanf("%d ",&a[i][j]);
+		}
+	}
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			printf("%d ",a[i][j]);
+		}
+	}
+}
