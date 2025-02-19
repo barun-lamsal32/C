33289f9748d954c6ebbcb9d036ee9be8f54d64f1
barun-lamsal32 <barunlamsal516@gmail.com>
2025-02-19 22:45:35 +0545

    Create to compare strings using strcmp function.cpp

---
+++ to compare strings using strcmp function.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+#include<string.h>
+main()
+{
+	char s[30]="Hari",c[30]="Krishna";
+	if(strcmp(s,c)==0)
+printf("the strings are equal");
+else
+printf("the strings are not equal");
+	
+}
