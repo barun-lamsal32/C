077119f9b27c1305a1a0141429f8414796fa2e65
barun-lamsal32 <barunlamsal516@gmail.com>
2025-02-19 22:43:39 +0545

    Create to check entered character is vowel or not uising vowel.cpp

---
+++ to check entered character is vowel or not uising vowel.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+main()
+{
+	char ch;
+	scanf("%c",&ch);
+	switch(ch)
+	{
+		case 'A':
+			case 'E':
+				case 'I':
+					case 'O':
+						case 'U':
+							printf("it is vowel");
+							break;
+							default:
+								printf("It is not vowel");
+								
+					
+				
+	}
+}
