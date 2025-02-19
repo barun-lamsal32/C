0682df070ccfd06984a2df6523c59d73b3dc8ca9
barun-lamsal32 <barunlamsal516@gmail.com>
2025-02-19 22:38:58 +0545

    Create the use of escape sequence.cpp

---
+++ the use of escape sequence.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+	char name[30],add[40];
+int 	roll;
+printf("Enter the name address and rollno");
+scanf("%s %s %d",name,add,&roll);
+printf("The name and address is %s\t %s\n ",name,add);
+printf("the rollno is %d ",roll);
+
+}
