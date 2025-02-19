1fef3c9a65499bb1b2442e4b98bfde622601d0e3
barun-lamsal32 <barunlamsal516@gmail.com>
2025-02-19 22:53:03 +0545

    Create to find even  numbers in given range using recursion.cpp

---
+++ to find even  numbers in given range using recursion.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+void even(int l, int u);
+
+int main() {
+    int l, u;
+    scanf("%d %d", &l, &u);
+    even(l, u);
+    return 0;
+}
+
+void even(int l, int u) {
+    if (l > u) {
+        return;  // Base case: stop recursion when l exceeds u
+    }
+    
+    // If l is odd, adjust l to the next even number
+    if (l % 2 != 0) {
+        l++;
+    }
+
+    // Print the even numbers within the range
+    for (; l <= u; l += 2) {
+        printf("%d ", l);
+    }
+}
